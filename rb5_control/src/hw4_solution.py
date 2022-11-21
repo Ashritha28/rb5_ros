b95bf9b32146eda16c8e72ee5737b02b39d7da16
Sai Ashritha Kandiraju <saiashrithakandiraju@Sais-MacBook-Pro-2.local>
2022-11-21 01:02:47 -0800

    tuning

--- rb5_control/src/hw4_solution.py
+++ rb5_control/src/hw4_solution.py
@@ -241,7 +241,7 @@ if __name__ == "__main__":
 
     # init pid controller
     # pid = PIDcontroller(0.0185,0.0015,0.09)
-    pid = PIDcontroller(0.05, 0.0015, 0.09)
+    pid = PIDcontroller(0.04, 0.0015, 0.09)
 
     # init current state
     current_state = np.array([0.3, 0.45, 0.0])
