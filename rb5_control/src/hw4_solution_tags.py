62fa4bcecf6aef45171673400fd358c9af8d38c6
Sai Ashritha Kandiraju <saiashrithakandiraju@Sais-MacBook-Pro-2.local>
2022-11-23 15:11:51 -0800

    ambiguity intro

--- rb5_control/src/hw4_solution_tags.py
+++ rb5_control/src/hw4_solution_tags.py
@@ -199,7 +199,8 @@ def getCurrentPos(l, current_state, pid):
                         # _,_,ang = rotationMatrixToEulerAngles(rot_april)
                         # april_tag_loc = [trans_april[0], trans_april[1], ang]
                         print("Pose - ", pose)       
-                        currentError = pid.getError(wTa[:2, 3], pose[:2, 3])
+                        # currentError = pid.getError(wTa[:2, 3], pose[:2, 3])
+                        currentError = math.dist(wTa[:2, 3], pose[:2, 3])
                         print("Current Error - ", currentError)
                         if currentError < minError:
                             minError = currentError
